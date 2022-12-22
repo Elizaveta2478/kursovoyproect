25f49c0407e9c2e573be683162160d1becb53f7f
Elizaveta <105592130+Elizaveta2478@users.noreply.github.com>
2022-12-23 02:31:11 +0300

    Create index.js

---
+++ js/index.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){
+    $('.main__button-summer-text').on('click', function(){
+        $('.main__news-summer-text').toggle();
+    });
+});
+$(document).ready(function(){
+    $('.main__button-autumn-text').on('click', function(){
+        $('.main__news-autumn-text').toggle();
+    });
+});
+$(document).ready(function(){
+    $('.main__button-winter-text').on('click', function(){
+        $('.main__news-winter-text').toggle();
+    });
+});
+$(document).ready(function(){
+    $('.main__button-year-text').on('click', function(){
+        $('.main__news-year-text').toggle();
+    });
+});
+$(document).ready(function(){
+    $('.header__button-menu').on('click', function(){
+        $('.header__aside').toggle();
+    });
+});
